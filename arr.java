ff8249fee4cd003f78f6aac9e9cd5ec9ff8c4fd0
priyadharshinidurai <105126899+priyadharshinidurai@users.noreply.github.com>
2022-05-23 23:53:49 -0700

    Add files via upload

---
+++ arr.java
@@ -0,0 +1,18 @@
+package array;
+public class arr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr= {12,23,34,45,56,67};
+		System.out.println(arr[1]);
+		System.out.println(arr[3]);
+		arr[0]+=1;		
+		arr[1]+=1;	
+		arr[2]+=1;
+		arr[3]+=1;		
+for(int num:arr)
+	System.out.println(num);
+	}
+
+}
