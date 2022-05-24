2556c1dc123dd7899a8df4421e9f57cf89db5cc0
priyadharshinidurai <105126899+priyadharshinidurai@users.noreply.github.com>
2022-05-24 00:02:36 -0700

    Add files via upload

---
+++ childclass.java
@@ -0,0 +1,27 @@
+package innerclasspack;
+
+ class childclass {
+
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          }*/
+	   
+	
+	/*public static void main (String[] args)
+	 {
+		Baseclass baseClass=new Baseclass()
+         {
+			@Override
+			public void display()
+			{
+				System.out.print("Anonymous implemented");
+			}
+         };
+			baseClass.display();*/
+         }
+	
+       
+
+
+	
+	
